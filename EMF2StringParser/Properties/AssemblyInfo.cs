ee129e622575df71940a218f6483e03a40447ffa
Ryu Youngseok <ryu.youngseok0512@mediasol.kr>
2020-03-02 17:08:02 +0900

    Add nuspec file

--- EMF2StringParser/Properties/AssemblyInfo.cs
+++ EMF2StringParser/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
 [assembly: AssemblyTitle("EMF2StringParser")]
-[assembly: AssemblyDescription("Rough EMF to Text Parser. Only Supports some of record types which prints text.")]
+[assembly: AssemblyDescription("EMF to Text Parser. Only Supports some of record types which prints text.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("yu4321")]
 [assembly: AssemblyProduct("EMF2StringParser")]
 [assembly: AssemblyCopyright("Copyright © yu4321 2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("yu4321")]
 [assembly: AssemblyCulture("")]
 
 // ComVisible을 false로 설정하면 이 어셈블리의 형식이 COM 구성 요소에 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호가 자동으로 
 // 지정되도록 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
